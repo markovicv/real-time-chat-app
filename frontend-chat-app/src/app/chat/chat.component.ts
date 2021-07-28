0f725057754fd552ea2617f7d68ea9d7242e050f
markovicv <vukasinnauka@gmail.com>
2021-07-28 14:10:28 +0200

    implemented notifications when a new message has arrived

--- frontend-chat-app/src/app/chat/chat.component.ts
+++ frontend-chat-app/src/app/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MessageService } from '../message.service';
+import { Friend } from '../model/friends';
 
 @Component({
   selector: 'app-chat',
@@ -9,18 +10,26 @@ import { MessageService } from '../message.service';
 })
 export class ChatComponent implements OnInit {
 
+  messageToSend;
+
+
   constructor(public messageService:MessageService,private router:Router) { }
 
   ngOnInit(): void {
-    // this.messageService.initWebSocket();
+    this.messageService.initWebSocket();
     this.messageService.getAllFriendsList();
   }
 
   send(){
-    this.messageService.sendMessage();
+    this.messageService.sendMessage(this.messageToSend);
+    this.messageToSend="";
   }
 
 
+  openChat(friend:Friend){
+
+    this.messageService.openChat(friend);
+  }
 
   navigateToPrivateChat(){
     this.router.navigateByUrl("/chatApp");
@@ -30,4 +39,10 @@ export class ChatComponent implements OnInit {
     this.router.navigateByUrl("/publicChat");
   }
 
+  firstClicked(){
+    return this.messageService.isClicked;
+  }
+  getMyId(){
+    return Number(localStorage.getItem("currentId"));
+  }
 }
--- frontend-chat-app/src/app/message.service.ts
+++ frontend-chat-app/src/app/message.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { ChatMessage } from './model/chat-message';
 import { Friend } from './model/friends';
 
@@ -13,8 +14,10 @@ export class MessageService {
 
   public stompClient;
   public friendsList:Array<Friend> = [];
-
-
+  public currentChatMessages:Array<ChatMessage> =[];
+  public currentChatRoomId;
+  public currentFriend:Friend;
+  public isClicked:boolean = false;
 
   constructor(private httpClient:HttpClient) { }
 
@@ -24,6 +27,7 @@ export class MessageService {
 
     this.stompClient = Stomp.over(websocket);
     this.stompClient.connect({},this.onConnected,this.onError);
+    
 
 
   }
@@ -32,7 +36,15 @@ export class MessageService {
     console.log("uspesno konektovan!");
     console.log(localStorage.getItem("currentId"));
     this.stompClient.subscribe("/user/"+String(localStorage.getItem("currentId"))+"/queue/messages",incomingMsg=>{
-      
+      const chatMessage:ChatMessage = JSON.parse(incomingMsg.body);
+
+
+      if(this.currentFriend!=null && chatMessage.senderId == this.currentFriend.friendId){
+        this.currentChatMessages.push(chatMessage);
+      }
+      else{
+        this.newUnreadMessages(chatMessage.senderId);
+      }
     })
 
   }
@@ -41,24 +53,78 @@ export class MessageService {
 
   }
 
-  sendMessage(){
+  sendMessage(messageToSend:String  ){
     let msg = new ChatMessage();
-    let currentId = Number(localStorage.getItem("currentId"));
-    msg.chatId = -1;
-    msg.data = "RADDDDIII";
-    msg.senderId = currentId;
-    msg.receiverId =3;
-    
-    this.stompClient.send("/app/chat",{},JSON.stringify(msg));
+
+    msg.chatId = this.currentChatRoomId;
+    msg.data = messageToSend;
+    msg.senderId = Number(localStorage.getItem("currentId"));
+    msg.receiverId =this.currentFriend.friendId;
+
+
+    this.httpClient.post<ChatMessage>("http://localhost:9092/chat",msg).subscribe(data=>{
+
+      this.currentChatMessages.push(data);
+      this.stompClient.send("/app/chat",{},JSON.stringify(data));
+
+
+
+    });    
   }
 
   getAllFriendsList(){
     let currentUsernameId = Number(localStorage.getItem("currentId"));
 
     this.httpClient.get<any>("http://localhost:9092/friends/"+currentUsernameId).subscribe(data=>{
+
+      
+
       this.friendsList = data;
     })
   }
 
+  openChat(friend:Friend){
+    this.currentFriend = friend;
+    this.getAllMessagesBetweenUsers(Number(localStorage.getItem("currentId")),friend.friendId).subscribe(data=>{
+
+        this.currentChatMessages = data.messageDtoList;
+
+        this.isClicked=true;
+        this.currentChatRoomId = data.chatId
+        this.markMessagesAsDelivered(friend.friendId);        
+
+  
+
+    })
+
+  }
+
+
+  getAllMessagesBetweenUsers(senderId:number,receiverId:number):Observable<any>{
+    return this.httpClient.get<any>("http://localhost:9092/messages/"+receiverId+"/"+senderId);
+  }
+
+
+  markMessagesAsDelivered(senderId:number){
+    for(let friend of this.friendsList){
+      if(friend.friendId == senderId){
+        friend.numberOfUnreadMessages=0;
+      }
+    }
+  }
+  newUnreadMessages(senderId:number){
+    for(let friend of this.friendsList){
+      if(friend.friendId ==senderId){
+        friend.numberOfUnreadMessages+=1;
+      }
+    }
+  }
+  getFriend(){
+    for(let friend of this.friendsList){
+      if (friend.friendId == this.currentFriend.friendId){
+        return friend;
+      }
+    }
+  }
 
 }
