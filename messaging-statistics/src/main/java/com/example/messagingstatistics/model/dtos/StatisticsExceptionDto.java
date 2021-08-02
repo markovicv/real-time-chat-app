dd6fb740ebd2d446123aabac678c6c97fac20e4c
markovicv <vukasinnauka@gmail.com>
2021-08-02 14:02:26 +0200

    implemented exception handling in statistics service

---
+++ messaging-statistics/src/main/java/com/example/messagingstatistics/model/dtos/StatisticsExceptionDto.java
@@ -0,0 +1,16 @@
+package com.example.messagingstatistics.model.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StatisticsExceptionDto {
+
+    private String exceptionMessage;
+
+
+
+}
