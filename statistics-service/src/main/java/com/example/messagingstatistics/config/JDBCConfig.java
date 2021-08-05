934da1ee030b82d64fdcd5501f34bafec8c89329
markovicv <vukasinnauka@gmail.com>
2021-08-05 13:24:44 +0200

    implemented jdbc configuration for jdbc template

---
+++ statistics-service/src/main/java/com/example/messagingstatistics/config/JDBCConfig.java
@@ -0,0 +1,40 @@
+package com.example.messagingstatistics.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.DriverManager;
+
+@Configuration
+public class JDBCConfig {
+
+    @Value("${spring.mysql.connection}")
+    private String connectionUrl;
+
+    @Value("${spring.mysql.username}")
+    private String username;
+
+    @Value("${spring.mysql.password}")
+    private String password;
+
+
+
+
+    @Bean
+    DataSource dataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setUrl(connectionUrl);
+        driverManagerDataSource.setUsername(username);
+        driverManagerDataSource.setPassword(password);
+        return driverManagerDataSource;
+    }
+
+    @Bean
+    JdbcTemplate getJdbcTemplate(DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+}
