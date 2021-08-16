f12a7d47f01fb24b867b4cdb0ac2a4be55d87ffd
markovicv <vukasinnauka@gmail.com>
2021-08-16 19:06:35 +0200

    added eureka client annotation to services

--- auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
@@ -4,9 +4,10 @@ package com.example.authservice;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class AuthServiceApplication {
 
